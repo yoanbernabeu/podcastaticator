c0f2d597dc402a13a78b0ce07cd4d20715af6e0e
Yoan Bernabeu <yoan.bernabeu@gmail.com>
2025-01-23 21:42:06 +0100

    Suppression du workflow de déploiement GitHub Pages et simplification de la configuration Vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 
-export default defineConfig({
-  base: '/podcastaticator/', // Remplacez <repository-name> par le nom de votre dépôt GitHub
+export default {
   server: {
-    open: true,
-  },
-})
+    open: true
+  }
+}
